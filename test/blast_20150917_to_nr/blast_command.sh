929bc7ff5de3f2e1bc823bef3db0fafe41e9890b
mtop <mats@topel.se>
2015-09-18 18:28:45 +0200

    More CPUs are available on the high_mem queue. Running the analysis on 56 cores

--- test/blast_20150917_to_nr/blast_command.sh
+++ test/blast_20150917_to_nr/blast_command.sh
@@ -4,4 +4,4 @@
 #$ -cwd
 #$ -S /bin/bash
 
-blastx -query /nobackup/data11/Fucus_vesiculosus_genome_project/test/20150915/SORTED-length_Fucus_vesiculosus_20150915_novo.fa -db /state/partition1/db/ncbi/nr -num_alignments 10 -num_threads 48 -query_loc 1-5000 -out /state/partition2/mats/Fucus_vesiculosus_genome_project/test/blast_20150917_to_nr/F_vesiculosus_20150917_to_nr.BLASTx.txt
+blastx -query /nobackup/data11/Fucus_vesiculosus_genome_project/test/20150915/SORTED-length_Fucus_vesiculosus_20150915_novo.fa -db /state/partition1/db/ncbi/nr -num_alignments 10 -num_threads 56 -query_loc 1-5000 -out /state/partition2/mats/Fucus_vesiculosus_genome_project/test/blast_20150917_to_nr/F_vesiculosus_20150917_to_nr.BLASTx.txt
