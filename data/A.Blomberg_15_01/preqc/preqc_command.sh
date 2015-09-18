dae7e74e64b4b4140875f8e2e2be355ac8f4a9ac
mtop <mats@topel.se>
2015-09-18 10:19:50 +0200

    Data has moved. Correct path is now included in the script

--- data/A.Blomberg_15_01/preqc/preqc_command.sh
+++ data/A.Blomberg_15_01/preqc/preqc_command.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-FILE1=/nobackup/data11/data/Fucus_vesiculosus/A.Blomberg_15_01/best_practice_qc/P1887_101/fastq_trimmed/7_150521_AC6VGLANXX_P1887_101_1.fastq
-FILE2=/nobackup/data11/data/Fucus_vesiculosus/A.Blomberg_15_01/best_practice_qc/P1887_101/fastq_trimmed/7_150521_AC6VGLANXX_P1887_101_2.fastq
+#$ -cwd
+#$ -q high_mem
+#$ -S /bin/bash
+
+FILE1=/nobackup/data11/Fucus_vesiculosus_genome_project/data/A.Blomberg_15_01/best_practice_qc/P1887_101/fastq_trimmed/7_150521_AC6VGLANXX_P1887_101_1.fastq
+FILE2=/nobackup/data11/Fucus_vesiculosus_genome_project/data/A.Blomberg_15_01/best_practice_qc/P1887_101/fastq_trimmed/7_150521_AC6VGLANXX_P1887_101_2.fastq
 PREPROCESSED=Fucus_vesiculosus.preprocessed.fastq
 GENOME=Fucus_vesiculosus.preprocessed.preqc
 CPU=8
