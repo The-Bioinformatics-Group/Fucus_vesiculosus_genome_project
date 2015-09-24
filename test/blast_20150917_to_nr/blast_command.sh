1d85d737c9bb41da3afcd6885ae24171dab84c35
mtop <mats@topel.se>
2015-09-24 08:10:30 +0200

    First commit

--- test/blast_20150917_to_nr/blast_command.sh
+++ test/blast_20150917_to_nr/blast_command.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#$ -q high_mem
+#$ -q node0
 #$ -cwd
 #$ -S /bin/bash
 
-blastx -query /nobackup/data11/Fucus_vesiculosus_genome_project/test/20150915/SORTED-length_Fucus_vesiculosus_20150915_novo.fa -db /state/partition1/db/ncbi/nr -num_alignments 10 -num_threads 56 -query_loc 1-5000 -out /state/partition2/mats/Fucus_vesiculosus_genome_project/test/blast_20150917_to_nr/F_vesiculosus_20150917_to_nr.BLASTx.txt
+blastx -query /nobackup/data11/Fucus_vesiculosus_genome_project/test/20150917/SORTED-length_Fucus_vesiculosus_20150917_novo.fa -db /state/partition1/db/ncbi/nr/nr -num_alignments 10 -num_threads 40 -query_loc 1-5000 -out /state/partition2/mtop/Fucus_vesiculosus_genome_project/test/blast_20150917_to_nr/F_vesiculosus_20150917_to_nr.BLASTx.txt
