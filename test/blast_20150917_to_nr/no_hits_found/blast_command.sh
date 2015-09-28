942fb7a65c4acfc8e82f007bfdbd584a4fe835ed
mtop <mats@topel.se>
2015-09-28 15:04:58 +0200

    Corrected the path to the output file

--- test/blast_20150917_to_nr/no_hits_found/blast_command.sh
+++ test/blast_20150917_to_nr/no_hits_found/blast_command.sh
@@ -4,4 +4,4 @@
 #$ -cwd
 #$ -S /bin/bash
 
-blastx -query /state/partition2/mtop/Fucus_vesiculosus_genome_project/test/blast_20150917_to_nr/no_hits_found/no_hits_found.fst -db /state/partition1/db/ncbi/nr/nr -num_alignments 10 -num_threads 40 -out /state/partition2/mtop/Fucus_vesiculosus_genome_project/test/blast_20150917_to_nr/F_vesiculosus_20150917_No-hits-found_to_nr.BLASTx.txt
+blastx -query /state/partition2/mtop/Fucus_vesiculosus_genome_project/test/blast_20150917_to_nr/no_hits_found/no_hits_found.fst -db /state/partition1/db/ncbi/nr/nr -num_alignments 10 -num_threads 40 -out /state/partition2/mtop/Fucus_vesiculosus_genome_project/test/blast_20150917_to_nr/no_hits_found/F_vesiculosus_20150917_No-hits-found_to_nr.BLASTx.txt
