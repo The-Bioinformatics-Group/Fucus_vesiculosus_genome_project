adbb5b606567c29350962eaee91f7828fc682e34
mtop <mats@topel.se>
2015-10-01 17:47:37 +0200

    Removed the num_alignments option

--- test/blast_20150917_to_nr/Cafeteria_roenbergensis_virus_BV-PW1/blast_command.sh
+++ test/blast_20150917_to_nr/Cafeteria_roenbergensis_virus_BV-PW1/blast_command.sh
@@ -5,4 +5,4 @@
 #$ -S /bin/bash
 #$ -pe mpich 38
 
-blastn -query /nobackup/data11/Fucus_vesiculosus_genome_project/test/blast_20150917_to_nr/Cafeteria_roenbergensis_virus_BV-PW1/GU244497.fasta -db /state/partition2/mtop/Fucus_vesiculosus_genome_project/test/20150917/Fucus_vesiculosus_20150917_novo.fa -num_alignments 10 -num_threads 38 -out /nobackup/data11/Fucus_vesiculosus_genome_project/test/blast_20150917_to_nr/Cafeteria_roenbergensis_virus_BV-PW1/Cafeteria_roenbergensis_virus_BV-PW1_20_20150917.BLASTn.txt
+tblastx -query /nobackup/data11/Fucus_vesiculosus_genome_project/test/blast_20150917_to_nr/Cafeteria_roenbergensis_virus_BV-PW1/GU244497.fasta -db /state/partition2/mtop/Fucus_vesiculosus_genome_project/test/20150917/Fucus_vesiculosus_20150917_novo.fa -num_threads 38 -out /nobackup/data11/Fucus_vesiculosus_genome_project/test/blast_20150917_to_nr/Cafeteria_roenbergensis_virus_BV-PW1/Cafeteria_roenbergensis_virus_BV-PW1_20_20150917.tBLASTx.txt
