d96ae0d0527d1abdadbcd112b952e7cc9dc2eeee
mtop <mats@topel.se>
2015-10-02 15:42:25 +0200

    Update

--- test/littorina_contamination/blast_command.sh
+++ test/littorina_contamination/blast_command.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-#$ -q node0
+#$ -q high_mem
+#$ -pe mpich 54
 #$ -cwd
 #$ -S /bin/bash
 
-blastn -query ../blast_20150917_to_nr/unknown_contigs/unknown_contigs.fst -db ../../data/Littorina/contigsAbove500.fasta -num_alignments 10 -num_threads 40 -outfmt '7 std qlen slen' -out unknown_contigs_to_Litorina-contigsAbove500.BLASTn.txt
+blastn -query ../blast_20150917_to_nr/unknown_contigs/unknown_contigs.fst -db ../../data/Littorina/contigsAbove500.fasta -num_alignments 10 -num_threads 54 -outfmt '7 std qlen slen' -out unknown_contigs_to_Litorina-contigsAbove500.BLASTn.txt
