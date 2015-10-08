90038de2e0eab2e6d6d154816d946509e356f4cb
mtop <mats@topel.se>
2015-10-08 23:27:32 +0200

    First commit

---
+++ test/blast_CA._P._Ectocarpi_to_20150917/blast_command.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#$ -q high_mem
+#$ -cwd
+#$ -S /bin/bash
+#$ -pe mpich 40
+
+blastx -query /nobackup/data11/Fucus_vesiculosus_genome_project/data/Ectocarpus_siliculosus/HG966617.fasta -db /nobackup/data11/Fucus_vesiculosus_genome_project/test/20150917/Fucus_vesiculosus_20150917_novo.fa -num_threads 40 -out ca._p._ectocarpi_to_20150917.BLASTn.txt
