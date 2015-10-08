3d9141962974c1ca800eeb87408ae2715883125f
mtop <mats@topel.se>
2015-10-08 23:59:27 +0200

    Changed to blastn

--- test/blast_CA._P._Ectocarpi_to_20150917/blast_command.sh
+++ test/blast_CA._P._Ectocarpi_to_20150917/blast_command.sh
@@ -5,4 +5,4 @@
 #$ -S /bin/bash
 #$ -pe mpich 40
 
-blastx -query /nobackup/data11/Fucus_vesiculosus_genome_project/data/Ectocarpus_siliculosus/HG966617.fasta -db /nobackup/data11/Fucus_vesiculosus_genome_project/test/20150917/Fucus_vesiculosus_20150917_novo.fa -num_threads 40 -out ca._p._ectocarpi_to_20150917.BLASTn.txt
+blastn -query /nobackup/data11/Fucus_vesiculosus_genome_project/data/Ectocarpus_siliculosus/HG966617.fasta -db ../20150917/Fucus_vesiculosus_20150917_novo.fa -num_threads 40 -out ca._p._ectocarpi_to_20150917.BLASTn.txt
