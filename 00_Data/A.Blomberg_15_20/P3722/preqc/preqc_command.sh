0a404532e851c6bf27c4b3dc040b77bbe76a9ab4
mtop <mats@topel.se>
2016-09-16 10:19:02 +0200

    asdf

--- 00_Data/A.Blomberg_15_20/P3722/preqc/preqc_command.sh
+++ 00_Data/A.Blomberg_15_20/P3722/preqc/preqc_command.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 #$ -cwd
-#$ -q high_mem
+#$ -q Annotation-2
 #$ -S /bin/bash
+#$ -pe mpich 20
 
 module load SGA/v0.10.15
 
@@ -10,9 +11,8 @@ FILE1=../best_practice_qc/P3722_201/fastq_trimmed/P3722_201_S1_L001_R1_001.fastq
 FILE2=../best_practice_qc/P3722_201/fastq_trimmed/P3722_201_S1_L001_R2_001.fastq
 PREPROCESSED=Fucus_vesiculosus_A.Blomberg_15_20_MPL.preprocessed.fastq
 GENOME=Fucus_vesiculosus_A.Blomberg_15_20_MPL.preprocessed.preqc
-CPU=8
 
 sga preprocess --pe-mode 1 $FILE1 $FILE2 > $PREPROCESSED
-sga index -a ropebwt --no-reverse -t $CPU $PREPROCESSED
-sga preqc -t $CPU $PREPROCESSED > $GENOME
-sga-preqc-report.py $GENOME /usr/local/bin/SGA-0.10.13/src/examples/preqc/*.preqc
+sga index -a ropebwt --no-reverse -t $NSLOTS $PREPROCESSED
+sga preqc -t $NSLOTS $PREPROCESSED > $GENOME
+sga-preqc-report.py $GENOME /usr/local/packages/sga-0.10.15/src/examples/preqc/*.preqc
