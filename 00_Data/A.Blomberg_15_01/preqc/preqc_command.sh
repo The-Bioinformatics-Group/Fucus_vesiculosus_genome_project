de182dd552df079c7280525767d6105730ec756b
mtop <mats@topel.se>
2016-09-16 10:07:41 +0200

    Modified to use  and with corrected paths to fastq files

--- 00_Data/A.Blomberg_15_01/preqc/preqc_command.sh
+++ 00_Data/A.Blomberg_15_01/preqc/preqc_command.sh
@@ -1,16 +1,14 @@
-#!/bin/bash
-
 #$ -cwd
-#$ -q high_mem
+#$ -q Annotation-3
 #$ -S /bin/bash
+#$ -pe mpich 20
 
-FILE1=/nobackup/data11/Fucus_vesiculosus_genome_project/data/A.Blomberg_15_01/best_practice_qc/P1887_101/fastq_trimmed/7_150521_AC6VGLANXX_P1887_101_1.fastq
-FILE2=/nobackup/data11/Fucus_vesiculosus_genome_project/data/A.Blomberg_15_01/best_practice_qc/P1887_101/fastq_trimmed/7_150521_AC6VGLANXX_P1887_101_2.fastq
-PREPROCESSED=Fucus_vesiculosus.preprocessed.fastq
-GENOME=Fucus_vesiculosus.preprocessed.preqc
-CPU=8
+FILE1=../best_practice_qc/P1887_101/fastq_trimmed/7_150521_AC6VGLANXX_P1887_101_1.fastq
+FILE2=../../best_practice_qc/P1887_101/fastq_trimmed/7_150521_AC6VGLANXX_P1887_101_2.fastq
+PREPROCESSED=F_ves_A.Blomberg_15_01_180bp.preprocessed.fastq
+GENOME=F_ves_A.Blomberg_15_01_180bp.preprocessed.preqc
 
 sga preprocess --pe-mode 1 $FILE1 $FILE2 > $PREPROCESSED
-sga index -a ropebwt --no-reverse -t $CPU $PREPROCESSED
-sga preqc -t $CPU $PREPROCESSED > $GENOME
+sga index -a ropebwt --no-reverse -t $NSLOTS $PREPROCESSED
+sga preqc -t $NSLOTS $PREPROCESSED > $GENOME
 sga-preqc-report.py $GENOME /usr/local/bin/SGA-0.10.13/src/examples/preqc/*.preqc
