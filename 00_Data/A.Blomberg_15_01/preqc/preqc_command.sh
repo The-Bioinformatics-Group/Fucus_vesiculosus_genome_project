c93d19011241c8485079c4c9c2cb97bb7f9c2f62
mtop <mats@topel.se>
2016-09-16 12:15:20 +0200

    Added the correct pathe to the latest version of sga

--- 00_Data/A.Blomberg_15_01/preqc/preqc_command.sh
+++ 00_Data/A.Blomberg_15_01/preqc/preqc_command.sh
@@ -3,6 +3,8 @@
 #$ -S /bin/bash
 #$ -pe mpich 20
 
+module load SGA/v0.10.15
+
 FILE1=../best_practice_qc/P1887_101/fastq_trimmed/7_150521_AC6VGLANXX_P1887_101_1.fastq
 FILE2=../../best_practice_qc/P1887_101/fastq_trimmed/7_150521_AC6VGLANXX_P1887_101_2.fastq
 PREPROCESSED=F_ves_A.Blomberg_15_01_180bp.preprocessed.fastq
@@ -11,4 +13,4 @@ GENOME=F_ves_A.Blomberg_15_01_180bp.preprocessed.preqc
 sga preprocess --pe-mode 1 $FILE1 $FILE2 > $PREPROCESSED
 sga index -a ropebwt --no-reverse -t $NSLOTS $PREPROCESSED
 sga preqc -t $NSLOTS $PREPROCESSED > $GENOME
-sga-preqc-report.py $GENOME /usr/local/bin/SGA-0.10.13/src/examples/preqc/*.preqc
+sga-preqc-report.py $GENOME /usr/local/packages/sga-0.10.15/src/examples/preqc/*.preqc
